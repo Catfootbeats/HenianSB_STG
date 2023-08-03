15cdd9337de761832b723d4ef6974c416b3f8ad0
Catfootbeats <74121308+Catfootbeats@users.noreply.github.com>
2023-08-03 09:28:20 +0800

    fix package bugs

--- config_loader.py
+++ config_loader.py
@@ -7,16 +7,17 @@ import settings
 import tools
 
 CFG_EXAMPLE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config_example.ini')
+CFG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
 
 
 class CfgLoader:
     def __init__(self):
         self.settings = settings.Settings()
         try:
-            f = open('./config.ini')
+            f = open(CFG_PATH)
             f.close()
             conf = ConfigParser()
-            conf.read('config.ini', encoding='utf-8')
+            conf.read(CFG_PATH, encoding='utf-8')
             for key, value in conf['default'].items():
                 if key == 'bg_color':
                     setattr(self.settings, key, str(value))
@@ -30,9 +31,9 @@ class CfgLoader:
         except IOError:
             try:
                 tools.debug('no file')
-                copyfile(CFG_EXAMPLE_PATH, './config.ini')
+                copyfile(CFG_EXAMPLE_PATH, CFG_PATH)
             except IOError:
-                tkinter.messagebox.showwarning('无法访问文件夹！')
+                tkinter.messagebox.showwarning(message='无法访问文件夹，请尝试换个文件夹。')
 
     def load(self) -> settings.Settings:
         return self.settings
